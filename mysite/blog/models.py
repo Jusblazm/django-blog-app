650da29efa8f1421dbc1622199b4338c2fe40b6c
Ryan <ryanlarkin0@gmail.com>
2024-07-25 14:30:28 -0700

    blog post sharing

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 
 # Create your models here.
@@ -12,21 +13,17 @@ class Post(models.Model):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
-    author = models.ForeignKey(
-                               settings.AUTH_USER_MODEL,
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,
                                on_delete=models.CASCADE,
-                               related_name='blog_posts'
-                               )
+                               related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(
-                              max_length=2,
+    status = models.CharField(max_length=2,
                               choices=Status,
-                              default=Status.DRAFT
-                              )
+                              default=Status.DRAFT)
     objects = models.Manager() # the default manager
     published = PublishedManager() # custom manager
 
@@ -36,3 +33,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year,
+                                                 self.publish.month,
+                                                 self.publish.day,
+                                                 self.slug])
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,23 +1,74 @@
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404, render
+from django.views.generic import ListView
 from .models import Post
+from .forms import EmailPostForm
 
 # Create your views here.
-def post_list(request):
-    posts = Post.published.all()
-    return render(
-                  request,
-                  'blog/post/list.html',
-                  {'posts': posts}
-                  )
+class PostListView(ListView):
+    """
+    Alternative post list view
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
-def post_detail(request, id):
-    post = get_object_or_404(
-                             Post,
-                             id=id,
-                             status=Post.Status.PUBLISHED
-                             )
-    return render(
-                  request,
+def post_list(request):
+    post_list = Post.published.all()
+    # pagination with 3 posts per page
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # if page_number is not an integer get the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # if page_number is out of range get last page of results
+        posts = paginator.page(paginator.num_pages)
+    return render(request,
+                  'blog/post/list.html',
+                  {'posts': posts})
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post,
+                             status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    return render(request,
                   'blog/post/detail.html',
-                  {'post': post}
-                  )
+                  {'post': post})
+
+def post_share(request, post_id):
+    # retrieve post by id
+    post = get_object_or_404(Post,
+                             id=post_id,
+                             status=Post.Status.PUBLISHED)
+    sent = False
+    if request.method == 'POST':
+        # form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # form fields passed validation
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = (f"{cd['name']} ({cd['email']}) "
+                       f"recommends you read {post.title}")
+            message = (f"Read {post.title} at {post_url}\n\n"
+                       f"{cd['name']}\'s comments: {cd['comments']}")
+            send_mail(subject=subject,
+                      message=message,
+                      from_email=None,
+                      recipient_list=[cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request,
+                    'blog/post/share.html',
+                    {'post': post,
+                     'form': form,
+                     'sent': sent})
