6b496fbdbbbb453315d0698a424de4cb2645a58c
Ryan <ryanlarkin0@gmail.com>
2024-08-12 16:38:59 -0700

    psycopg=3.2.1

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -87,6 +87,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
